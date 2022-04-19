4096b6daa03ca5be66cf17573fd477d135cb033c
Randy <randy20gatcha@gmail.com>
2022-04-19 18:05:23 +0800

    additional

--- src/views/ClientView.vue
+++ src/views/ClientView.vue
@@ -1,12 +1,35 @@
 <template>
+ <h1>Employee List</h1>  
+   <!-- <input type="text" placeholder="Enter employee id" > -->
+   <!-- <button @click="getUser">Click</button> -->
    <div>
-       <h1>Employee details</h1>
+     <div v-for="user in getUser()" :key="user.id" class="user">
+       <p>{{ user.firstName }}</p> 
+     </div>
    </div>
+
 </template>
 
 <script>
-export default {
+import { ref } from '@vue/reactivity';
+import { getDocs } from 'firebase/firestore';
+import { employees } from '@/firebase';
 
+export default {
+    setup() {
+      const users = [];
+          
+       const getUser = () => {
+         getDocs(employees).then((snapshot) => {          
+           snapshot.docs.forEach((doc) => {
+             users.push({ ...doc.data(), id: doc.id });
+           })
+           console.log(users);
+         })        
+       }
+       return { users, getUser }
+    }
+   
 }
 </script>
 
