612378dc81d52d60f59dfefa0072e53355adbf0d
Randy <randy20gatcha@gmail.com>
2022-04-20 13:05:01 +0800

    additional

--- src/views/ClientView.vue
+++ src/views/ClientView.vue
@@ -2,11 +2,14 @@
  <h1>Employee List</h1>  
    <!-- <input type="text" placeholder="Enter employee id" > -->
    <!-- <button @click="getUser">Click</button> -->
-   <div>
+   <div v-if="users">
      <div v-for="user in users" :key="user.firstName">
        <p>{{ user.firstName }}</p> 
-         
+       <p>{{ user.designation }}</p>        
      </div>
+   </div>
+   <div v-else>
+     <h3>Loading data...</h3>
    </div>
 
 </template>
@@ -27,10 +30,14 @@ export default {
     methods: {
       async getData() {
         const fetchData = await getDocs(employees);
-        const users = [];
+        let users = [];
         fetchData.forEach((user) => {
-          users.push(user.data());
+          let userId = user.data();
+          userId.id = user.id;
+          users.push(userId);
+          
         })
+        console.log(users);
         this.users = users;
       }
     },
