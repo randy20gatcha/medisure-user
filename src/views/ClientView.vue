b5b500f4955ff8f4aa3349d84b13af1194aa2dd0
Randy <randy20gatcha@gmail.com>
2022-04-20 16:47:21 +0800

    add-on

--- src/views/ClientView.vue
+++ src/views/ClientView.vue
@@ -2,15 +2,27 @@
  <h1>Employee List</h1>  
    <!-- <input type="text" placeholder="Enter employee id" > -->
    <!-- <button @click="getUser">Click</button> -->
-   <div v-if="users">
-     <div v-for="user in users" :key="user.firstName">
-       <p>{{ user.firstName }}</p> 
-       <p>{{ user.designation }}</p>        
+   <div>
+     <div>
+        <table class="table table-bordered">
+            <thead class="table-dark">
+                <tr>
+                  <th>Name</th>
+                  <th>Designation</th>
+                  <th>Details</th>
+                </tr>
+            </thead>
+            <tbody v-for="user in users" :key="user.firstName">
+              <tr>
+                <td>{{ user.firstName }}</td>
+                <td>{{ user.designation }}</td>
+                <td><router-link :to="{ path: '/details' }">Details</router-link></td>
+              </tr>
+            </tbody>
+        </table>  
      </div>
    </div>
-   <div v-else>
-     <h3>Loading data...</h3>
-   </div>
+   
 
 </template>
 
@@ -48,6 +60,16 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+a {
+  background-color: rgb(69, 69, 128);
+  color: white;
+  font-size: small;
+  padding: 8px 10px;
+  text-decoration: none; 
+  border-radius: 5px; 
+}
+a:hover {
+  background-color: rgb(67, 67, 94);
+}
 </style>
